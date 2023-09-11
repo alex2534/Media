e8c6e91f3bf3fc069e33041460b856f8ddf30914
alex2534 <alex2534@outlook.com>
2023-09-11 20:12:51 -0300

    Creating the AlbumsList and ExpendablePanel

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,6 +2,8 @@ import { GoTrash } from "react-icons/go";
 import Button from "./Button";
 import { removeUser } from "../store";
 import { useThunk } from "../hooks/useThunk";
+import ExpendablePanel from "./ExpendablePanel";
+import AlbumsList from "./AlbumsList";
 
 function UsersListeItem({ user }) {
   const [doRemoveUser, isLoading, error] = useThunk(removeUser);
@@ -10,18 +12,21 @@ function UsersListeItem({ user }) {
     doRemoveUser(user);
   };
 
+  const header = (
+    <>
+      {" "}
+      <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+        <GoTrash />
+      </Button>
+      {error && <div>Error deleting user.</div>}
+      {user.name}
+    </>
+  );
+
   return (
-    <div className="mb-2 border rounded">
-      <div className="flex flex-row items-center justify-between">
-        <div className="flex p-2 justify-between items-center cursor-pointer">
-          <Button className="mr-3" loading={isLoading} onClick={handleClick}>
-            <GoTrash />
-          </Button>
-          {error && <div>Error deleting user.</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+    <ExpendablePanel header={header}>
+      <AlbumsList user={user} />
+    </ExpendablePanel>
   );
 }
 
